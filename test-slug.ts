35795097fb33dd89079ffb6708162614292e7e8a
hackernithu <hackernithu@example.com>
2026-07-14 23:36:16 -0400

    Remove temporary scratch files breaking the typescript build

--- test-slug.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function run() {
-  const p = await prisma.product.findFirst({ select: { slug: true } });
-  console.log("First product slug:", p?.slug);
-}
-run();
